8d3503de091af67265c95ee5f13fa22cf566882b
Way4ward Adefuwa <Way4ward Adefuwa@DESKTOP-OC7G3GQ>
2020-02-01 15:04:51 +0100

    Initial Upload

---
+++ src/AssignmentOptimal.java
@@ -0,0 +1,34 @@
+
+
+import java.util.Vector;
+
+
+/**
+ * AssignmentOptimal.java
+ * TODO: 
+ *
+ * @author way4ward
+ */
+
+public class AssignmentOptimal {
+	public double Solve(double[][] DistMatrix,
+			Vector<Integer> Assignment) {
+		int N = DistMatrix.length; // number of columns (tracks)
+		int M = DistMatrix[0].length; // number of rows (measurements)
+		int dim =  Math.max(N, M);
+
+		// Init
+		int[] match = new int[dim];
+
+		HungarianAlg3 b = new HungarianAlg3(DistMatrix);
+		match = b.execute();
+		
+		// form result
+		Assignment.clear();
+		for (int x = 0; x < N; x++) {
+			Assignment.add(match[x]);
+		}
+		
+		return b.computeCost(DistMatrix, match);
+	}
+}
